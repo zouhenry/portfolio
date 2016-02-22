60b03def3330d1dbbb2068e8f17247c40e81e9c4
Henry Zou <zouhenry@gmail.com>
2016-02-21 20:58:26 -0500

    * adding gulp tasks for ngAnnotate/concat/sourcemap src files into dest folder * adding .gitignore * adding following bower.json packages:   * Angular   * Angular Materials   * Lodash

---
+++ app/src/app.module.js
@@ -0,0 +1,14 @@
+/**
+ * Created by hzou on 2/21/16.
+ */
+/*========================================
+ =              APP START                =
+ ========================================*/
+
+angular
+  .module('portfolio', [])
+  .run(run);
+
+function run($filter) {
+
+}
